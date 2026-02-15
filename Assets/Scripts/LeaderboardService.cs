c3c055a297ece9c5b0b8c7605fb68aa4d9370026
mirtlbu <bubuzoglo@gmail.com>
2026-02-15 18:19:34 +0200

    leaderboard fetch from server added

--- Assets/Scripts/LeaderboardService.cs
+++ Assets/Scripts/LeaderboardService.cs
@@ -1,31 +1,50 @@
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 using UnityEngine;
 
+
 public class LeaderboardService : MonoBehaviour
 {
-   [Serializable]
+    [Serializable]
     public class PlayerScore
     {
         public string playerName;
         public int score;
     }
+
+    [Serializable]
+    public class LeaderboardResponse
+    {
+        public List<PlayerScore> leaderboard;
+    }
+
+    private static HttpClient httpClient = new HttpClient();
+
+    private static readonly string leaderboardUrl = "https://jutskat.fi/wp-content/3d/borb/leaderboard_resp.json";
+
+
     public async Task<List<PlayerScore>> GetLeaderboardAsync()
     {
-        await Task.Delay(2000);
-
-        List<PlayerScore> fakeData = new List<PlayerScore>();
-
-        for (int i = 0; i < 10; i++)
+        try
         {
-            fakeData.Add(new PlayerScore
-            {
-                playerName = $"Player_{i + 1}",
-                score = UnityEngine.Random.Range(0, 10000)
-            });
-        }
+            Debug.Log("Requesting leaderboard...");
 
-        return fakeData;
+            HttpResponseMessage response = await httpClient.GetAsync(leaderboardUrl);
+            response.EnsureSuccessStatusCode();
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            LeaderboardResponse result =
+                JsonUtility.FromJson<LeaderboardResponse>(json);
+
+            return result.leaderboard;
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Leaderboard request failed: {ex.Message}");
+            return new List<PlayerScore>();
+        }
     }
 }
